372fdabbcdec4764b10ada881e8500ba44560b64
Muhammad Ahsan <mtahsan00@gmail.com>
2018-05-09 23:03:04 -0500

    Draws around hsv values, current green

--- PyCV.py
+++ PyCV.py
@@ -1,23 +1,47 @@
-import cv2
-import numpy as np
 
-cap = cv2.VideoCapture(0)
-#fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#out = cv2.VideoWriter('hellWorld.avi', fourcc, 20.0, (640,480))
+
+from collections import deque
+import numpy as np
+import cv2
+
+
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+
+camera = cv2.VideoCapture(0)
+
+
 while True:
-    ret, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    cv2.imshow('frame', frame)
-    lower_hsv = np.array([50,200,0])
-    upper_hsv = np.array([255,255,255])
-    mask = cv2.inRange(hsv, lower_hsv,upper_hsv)
-    result = cv2.bitwise_and(frame,frame,mask=mask)
-    cv2.imshow('mask', mask)
-    cv2.imshow('result', result)
-    #out.write(frame)
-#    cv2.line(cap, (0,0),(150,150), (0,0,0))
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-#out.release()
+
+	(grabbed, frame) = camera.read()
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+
+	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
+	if len(cnts) > 0:
+	
+
+		c = max(cnts, key=cv2.contourArea)
+		x,y,w,h = cv2.boundingRect(c)
+
+		if True:
+
+			cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+
+
+
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+
+	if key == ord("q"):
+		break
+
+camera.release()
 cv2.destroyAllWindows()
